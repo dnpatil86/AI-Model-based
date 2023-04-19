2673a6377846546616a3f6c4cf6b092ec1b37b55
dnpatil86 <dnpatil86@gmail.com>
2023-04-19 16:27:23 +0530

    FIRST COMMIT

---
+++ myapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.home, name="home"),
+    path("answer/", views.answer, name="answer"),
+]
---
+++ myapp/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+import openai
+
+# Set up OpenAI API credentials
+openai.api_key = "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"
+
+def home(request):
+    return render(request, "home.html")
+
+def answer(request):
+    if request.method == "POST":
+        question = request.POST["question"]
+        response = openai.Completion.create(
+            engine="text-davinci-003",
+            prompt=f"Answer the following question: {question}",
+            max_tokens=50,
+            n=1,
+            stop=None,
+            temperature=0.5,
+        )
+        answer = response.choices[0].text.strip()
+        return render(request, "answer.html", {"question": question, "answer": answer})
+    else:
+        return home(request)
